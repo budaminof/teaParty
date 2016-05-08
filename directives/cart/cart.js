c21b30811bf39f564226e9c27cac73875bdde0b1
bud aminof <budaminof@gmail.com>
2016-05-07 21:32:27 -0600

    computer froze

--- directives/cart/cart.js
+++ directives/cart/cart.js
@@ -1,11 +1,13 @@
 angular.module('teaParty')
-.directive('teaCart', [function () {
+.directive('teaCart', ['cartService', '$log', function (cartService, $log) {
 
   return {
     restrict: 'E',
     templateUrl: '/directives/cart/cart.html',
     scope: {},
-    link: function () {
+    link: function (scope, element, attr, fn) {
+      scope.cart = cartService.getCart();
+      $log.log('IN DIRECTIVE CART',scope.cart)
 
     }
   }
