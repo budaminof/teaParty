fe047bd874d80b76401d555f2cd5ad3277f3c7c6
bud aminof <budaminof@gmail.com>
2016-05-07 19:07:56 -0600

    search by name working. you can filer by specific property name in an object after the filer

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ angular.module('teaParty',[])
     return input / 100;
   }
 })
+.filter('inStock', function () {
+  return function (input){
+    if(input) return "Yes"
+    else return "No"
+  }
+})
